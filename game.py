9786b068df8717961cb3c607cca79f9a30ad48a7
aomoroeng <aomoroeng@gmail.com>
2026-08-24 11:19:01 -0500

    Added user

--- game.py
+++ game.py
@@ -1,12 +1,21 @@
 from random import choice
 
+RPS = ["ROCK", "PAPER", "SISSORS"]
+
 
 def main():
+    print(user())
     print(bot())
 
 
+def user():
+    while True:
+        user_inpt = input("> ")
+        if user_inpt in RPS:
+            return user_inpt
+
+
 def bot():
-    RPS = ["ROCK", "PAPER", "SISSORS"]
     return choice(RPS)
 
 
